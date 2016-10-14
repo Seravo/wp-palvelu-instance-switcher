0e4e83c76fefb6ebcc684675579e5662a99ed6ab
Christian Nikkanen <christian.nikkanen@gmail.com>
2016-10-14 11:43:19 +0300

    Rewrite script.js to remove jQuery dependency. Fixes #6

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
-(function($) {
-$(document).on('click', '#wp-admin-bar-wpis li > a', function(e) {
-  e.preventDefault();
-  var instance = $(this).attr('href').substr(1);
-  if (instance == 'exit') {
-    document.cookie = "wpp_shadow=;path=/";
-  } else {
-    document.cookie = "wpp_shadow=" + instance + ";path=/";
+(function(){
+  var links = document.querySelectorAll('#wp-admin-bar-wpis li > a');
+  var listener = function(e){
+    e.preventDefault();
+    var instance = e.target.getAttribute('href').substr(1);
+    if (instance === 'exit') {
+      document.cookie = "wpp_shadow=;path=/";
+    } else {
+      document.cookie = "wpp_shadow=" + instance + ";path=/";
+    }
+    location.reload();
+  };
+  
+  for(var i = 0; i < links.length; i++){
+    links[i].addEventListener('click', listener);
   }
-  location.reload();
-});
-})(jQuery);
+})();
